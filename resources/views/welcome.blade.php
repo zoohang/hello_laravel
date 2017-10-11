df6822a744bc9cbd6cff7d2ae5add96c2c781755
zoohang <136927705@qq.com>
2017-10-11 13:55:01 +0000

    test git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by yuan</div>
+                <div class="title">Hello Laravel! - by yuan zoohang</div>
             </div>
         </div>
     </body>
